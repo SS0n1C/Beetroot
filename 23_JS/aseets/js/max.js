bb492245cfe6a80d8e5198a19c6683a14e571228
ss0n1c <lls.s0n1cll@gamil.com>
2022-07-19 12:19:32 +0300

    JS start

---
+++ 23_JS/aseets/js/max.js
@@ -0,0 +1,12 @@
+let circle = prompt("what is the radius of the circle", "");
+const IIi= 3.14;
+alert(`area of a circle is ${IIi*(circle*circle)}`);
+
+let distans = prompt("What distant between city", "");
+let time = prompt("And how much do you want to get there?", "");
+alert(`Your speed must be ${distans/time}`);
+
+let money = prompt("Haw many dollar do you have?", "");
+const dollar = 29.60;
+const evro = 30.28;
+alert(`You have ${(money*dollar)/evro} evro`);
---
+++ 23_JS/aseets/js/script.js
@@ -0,0 +1,9 @@
+let yourName = prompt("what is your name?", "");
+alert(`Hello ${yourName}`);
+
+let age = prompt("when you born?", "");
+const data = 2022;
+alert(`your age is ${data-age}`);
+
+let square = prompt("What is the length of the square?", "");
+alert(`Perimeter of a square ${square*4}`);
