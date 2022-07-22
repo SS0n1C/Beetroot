30efa748958850000073b65e5b80f42a538a799e
ss0n1c <lls.s0n1cll@gamil.com>
2022-07-22 13:01:18 +0300

    add new les

--- 23_JS/aseets/js/script.js
+++
@@ -1,9 +0,0 @@
-let yourName = prompt("what is your name?", "");
-alert(`Hello ${yourName}`);
-
-let age = prompt("when you born?", "");
-const data = 2022;
-alert(`your age is ${data-age}`);
-
-let square = prompt("What is the length of the square?", "");
-alert(`Perimeter of a square ${square*4}`);
---
+++ 24_JS/assets/js/script.js
@@ -0,0 +1,45 @@
+let a = 0.1;
+let b = 0.2;
+console.log( (a + b).toFixed(1) );
+
+let num1 = "1";
+let num2 = 2;
+console.log(+num1 + num2);
+
+
+let flesh = prompt("What size of your flash drive", "");
+const file = 0.820;
+alert(`You can write ${Math.floor(flesh / file)} file`);
+
+let $ = prompt("Haw many $ you have?", "");
+let price = prompt("And what a price your chocolate?","");
+alert(`Hmm.. you can buy only ${Math.floor($ / price)} i am very sory`);
+alert(`But dont sad,beckause you still have ${($ % price).toFixed(2)}`);
+
+let number = prompt("Add you number", "");
+let zero = 0;
+let rever;
+while(number > 0){
+  rever = number % 10;
+  zero = zero * 10;
+  zero += rever;
+  number = Math.floor(number/10);
+}
+alert(+zero);
+
+let deposit = prompt("Haw many kesh do you have?","");
+let percent = 5 / 6;
+alert(`After 2 monts you will have ${(+deposit/100 * percent).toFixed(2)}`);
+
+/*2 && 0 && 3 - Оскільки є нуль, то вираз стає false і поверне 0.
+ 2 || 0 || 3 - Поверне перший правильний вираз  2.
+ 2 && 0 || 3 - 2 && 0 - є нуль, тому false. Покаже 3*/
+
+
+
+ 
+
+
+
+
+
